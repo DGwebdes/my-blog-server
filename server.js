7b3389b691f93c2a84257cbcc62c0d4b6caab683
DGwebdes <alan.bilik8@gmail.com>
2025-03-13 03:01:29 +0000

    updating dependencies

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-
-const express = require('express');
-const contentful = require('contentful');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const contentful = require("contentful");
+const cors = require("cors");
+require("dotenv").config();
 
 // Express server setup
 
@@ -17,20 +16,20 @@ app.use(cors());
 const client = contentful.createClient({
     space: process.env.SPACE,
     accessToken: process.env.ACCESS_TOKEN,
-  });
+});
 
-app.get('/api/posts', async (req, res) => {
+app.get("/api/posts", async (req, res) => {
     try {
         const response = await client.getEntries();
         res.json(response.items);
     } catch (err) {
-        res.status(500).json({ message: err});
+        res.status(500).json({ message: err });
         console.log(`Error in get request: ${err}`);
     }
-})
+});
 
-app.get('/api/posts/:postId', async (req, res) => {
-    const {postId} = req.params;
+app.get("/api/posts/:postId", async (req, res) => {
+    const { postId } = req.params;
     console.log("Fetching posts with ID: ", postId);
     try {
         const response = await client.getEntry(postId);
@@ -40,19 +39,21 @@ app.get('/api/posts/:postId', async (req, res) => {
                 postImage: response.fields.postImage.fields.file.url,
                 content: response.fields.content,
                 author: response.fields.author,
-                publishDate: new Date(response.fields.publishDate).toLocaleDateString(),
-            }
-            res.json(post)
+                publishDate: new Date(
+                    response.fields.publishDate
+                ).toLocaleDateString(),
+            };
+            res.json(post);
         } else {
             // Return a 404 error if the postId does not match any entry
-            res.status(404).json({message: "Post not found"});
+            res.status(404).json({ message: "Post not found" });
         }
     } catch (err) {
-        res.status(500).json({message: err});
-        console.log(`Could not Fetch Post ${err}`)
+        res.status(500).json({ message: err });
+        console.log(`Could not Fetch Post ${err}`);
     }
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
