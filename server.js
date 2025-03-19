c8676a0b2282735152835be4f2d41541c9995bc9
DGwebdes <alan.bilik8@gmail.com>
2025-03-19 16:44:04 +0000

    updating cors to ensure vault is allowed to fetch posts

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://dgwebdes.github.io/vault",
+    })
+);
 
 // app.use(express.json());
 
