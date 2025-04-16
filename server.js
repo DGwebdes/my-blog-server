f0b261840a198e0f7cc74c991eca332b7587e95a
DGwebdes <alan.bilik8@gmail.com>
2025-04-16 22:28:28 +0100

    updated packages. Last commit. Backend decoupled from frontend.

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const client = contentful.createClient({
 app.get("/api/posts", async (req, res) => {
     try {
         const response = await client.getEntries();
+
         res.json(response.items);
     } catch (err) {
         res.status(500).json({ message: err });
@@ -40,7 +41,7 @@ app.get("/api/posts/:postId", async (req, res) => {
                 content: response.fields.content,
                 author: response.fields.author,
                 publishDate: new Date(
-                    response.fields.publishDate
+                    response.fields.publishDate,
                 ).toLocaleDateString(),
             };
             res.json(post);
